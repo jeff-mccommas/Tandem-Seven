52326ac415e84d3b84728f70141c4ce7c6cadd32
Jeffrey  McCommas <jeffmccommas@gmail.com>
2017-12-12 18:00:42 -0700

    node element create function

---
+++ scripts.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jmccommas on 12/12/17 7:57 PM*/
+function createNode(element) {
+	return document.createElement(element);
+}
+
+function append(parent, el) {
+	return parent.appendChild(el);
+}
